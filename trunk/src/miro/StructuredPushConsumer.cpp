39e71bad2ab208546b4696af9220f7de5e3d0bc6
hutz <hutz@7138bf71-271b-42e2-acd4-47588d9ec08a>
2002-11-25 09:45:52 +0000

    catching exceptions on unbind

--- trunk/src/miro/StructuredPushConsumer.cpp
+++ trunk/src/miro/StructuredPushConsumer.cpp
@@ -89,9 +89,15 @@ namespace Miro
     Guard guard(connectedMutex_);
     if (connected_) {
       DBG(cout << "Disconnecting StructuredPushConsumer." << endl);
-      proxySupplier_->disconnect_structured_push_supplier();
-      consumerAdmin_->destroy();
-      connected_ = false;
+      try {
+	proxySupplier_->disconnect_structured_push_supplier();
+	consumerAdmin_->destroy();
+	connected_ = false;
+      }
+      catch (const CORBA::Exception & e) {
+	cerr << "StructuredPushConsumer::disconnect() CORBA exception on: " << endl
+	     << e << endl;
+      }
     }      
   }
 
