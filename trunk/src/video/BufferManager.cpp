9ee1c2ea7e0a67ed42a0d8c79c98cde1f33c8541
hutz <hutz@7138bf71-271b-42e2-acd4-47588d9ec08a>
2003-06-03 10:37:12 +0000

    another new file pair

---
+++ trunk/src/video/BufferManager.cpp
@@ -0,0 +1,143 @@
+// -*- c++ -*- ///////////////////////////////////////////////////////////////
+//
+// This file is part of Miro (The Middleware For Robots)
+//
+// (c) 2003
+// Department of Neural Information Processing, University of Ulm, Germany
+//
+// $Id$
+// 
+//////////////////////////////////////////////////////////////////////////////
+
+#include "BufferManager.h"
+
+#include "miro/Exception.h"
+#include "miro/ExceptionC.h"
+
+namespace Video
+{
+  BufferManager::BufferManager(unsigned int _buffers, 
+			       unsigned int _bufferSize,
+			       unsigned char * _memory) :
+    buffers_(_buffers),
+    bufferSize_(_bufferSize),
+    memory_(_memory),
+    owner_(false),
+    cond_(mutex_),
+    currentBuffer_(0),
+    bufferStatus_(buffers_)
+  {
+    if (memory_ == NULL) {
+      memory_ = new unsigned char[bufferSize_ * buffers_];
+      owner_ = true;
+    }
+
+    unsigned int offset = 0;
+    BufferVector::iterator first, last = bufferStatus_.end();
+    for (first = bufferStatus_.begin(); first != last; ++first) {
+      first->buffer = memory_ + offset;
+      offset += bufferSize_;
+    }
+  }
+
+  BufferManager::BufferManager(unsigned int _buffers, 
+			      unsigned char * _bufferAddr[], 
+			      unsigned char * _memory) :
+    buffers_(_buffers),
+    bufferSize_(0),
+    memory_(_memory),
+    owner_(false),
+    cond_(mutex_),
+    currentBuffer_(0),
+    bufferStatus_(buffers_)
+  {
+    unsigned int index = 0;
+    BufferVector::iterator first, last = bufferStatus_.end();
+    for (first = bufferStatus_.begin(); first != last; ++first) {
+      first->buffer = _bufferAddr[index];
+      ++index;
+    }
+  }
+
+  BufferManager::~BufferManager() 
+  {
+    cond_.broadcast();
+    if (owner_)
+      delete[] memory_;
+  }
+
+  unsigned int 
+  BufferManager::acquireNextWriteBuffer()
+  {
+    Miro::Guard guard(mutex_);
+    unsigned int index = currentBuffer_;
+    do {
+      ++index;
+      index %= buffers_;
+      if (bufferStatus_[index].state == BufferEntry::FREE) {
+	bufferStatus_[index].state = BufferEntry::WRITING;
+	return index;
+      }
+    } 
+    while (index != currentBuffer_);
+
+    throw Miro::Exception("No buffer available for writing.");
+  }
+
+  void 
+  BufferManager::releaseWriteBuffer(unsigned int _index)
+  {
+    Miro::Guard guard(mutex_);
+    if (bufferStatus_[_index].state == BufferEntry::WRITING) {
+      bufferStatus_[_index].readers = 0;
+      bufferStatus_[_index].state = BufferEntry::FREE;
+      currentBuffer_ = _index;
+      cond_.broadcast();
+    }
+    else
+      throw Miro::EOutOfBounds("Trying to free wrong buffer.");
+  }
+
+  unsigned int 
+  BufferManager::acquireCurrentReadBuffer()
+  {
+    Miro::Guard guard(mutex_);
+    if (bufferStatus_[currentBuffer_].state == BufferEntry::FREE ||
+	bufferStatus_[currentBuffer_].state == BufferEntry::READING ) {
+      ++bufferStatus_[currentBuffer_].readers;
+      bufferStatus_[currentBuffer_].state = BufferEntry::READING;
+      return currentBuffer_;
+    }
+
+    throw Miro::Exception("Current buffer not free.");
+  }
+   
+  unsigned int 
+  BufferManager::acquireNextReadBuffer()
+  {
+    Miro::Guard guard(mutex_);
+    cond_.wait();
+    if (bufferStatus_[currentBuffer_].state == BufferEntry::FREE ||
+	bufferStatus_[currentBuffer_].state == BufferEntry::READING ) {
+      ++bufferStatus_[currentBuffer_].readers;
+      bufferStatus_[currentBuffer_].state = BufferEntry::READING;
+      return currentBuffer_;
+    }
+
+    throw Miro::Exception("Next current buffer not free.");
+
+  }
+
+  void
+  BufferManager::releaseReadBuffer(unsigned int _index)
+  {
+    Miro::Guard guard(mutex_);
+    if (bufferStatus_[_index].readers == 1) {
+      bufferStatus_[_index].readers = 0;
+      bufferStatus_[_index].state = BufferEntry::FREE;
+    }
+    else if (bufferStatus_[_index].readers > 1) {
+      --bufferStatus_[_index].readers;
+    }
+  }
+};
---
+++ trunk/src/video/BufferManager.h
@@ -0,0 +1,85 @@
+// -*- c++ -*- ///////////////////////////////////////////////////////////////
+//
+// This file is part of Miro (The Middleware For Robots)
+//
+// (c) 2002
+// Department of Neural Information Processing, University of Ulm, Germany
+//
+// $Id$
+// 
+//////////////////////////////////////////////////////////////////////////////
+#ifndef Video_BufferManager_h
+#define Video_BufferManager_h
+
+#include "miro/Synch.h"
+
+#include <vector>
+
+namespace Video
+{
+  struct BufferEntry
+  {
+    enum State {FREE, WRITING, READING};
+
+    ACE_Time_Value time;
+    State state;
+    unsigned int readers;
+    unsigned char * buffer;
+
+    BufferEntry() : state(FREE), readers(0) {}
+  };
+
+  class BufferManager
+  {
+  public:
+    BufferManager(unsigned int _buffers, 
+		  unsigned int _bufferSize, 
+		  unsigned char * _memory = NULL);
+    BufferManager(unsigned int _buffers,
+		  unsigned char * _bufferAddr[], 
+		  unsigned char * _memory);
+    ~BufferManager();
+
+    unsigned char * bufferAddr(unsigned int _index);
+    const ACE_Time_Value& bufferTimeStamp(unsigned int _index) const;
+    void bufferTimeStamp(unsigned int _index, ACE_Time_Value const& _stamp);
+
+    unsigned int acquireNextWriteBuffer();
+    void releaseWriteBuffer(unsigned int _index);
+
+    unsigned int acquireCurrentReadBuffer();
+    unsigned int acquireNextReadBuffer();
+    void releaseReadBuffer(unsigned int _index);
+    
+  protected:
+    typedef std::vector<BufferEntry> BufferVector;
+
+    unsigned int buffers_;
+    unsigned int bufferSize_;
+    unsigned char * memory_;
+    bool owner_;
+
+    Miro::Mutex mutex_;
+    Miro::Condition cond_;
+    unsigned int currentBuffer_;
+    BufferVector bufferStatus_;
+  };
+
+  inline 
+  unsigned char * 
+  BufferManager::bufferAddr(unsigned int _index) {
+    return bufferStatus_[_index].buffer;
+  }
+  inline 
+  const ACE_Time_Value&
+  BufferManager::bufferTimeStamp(unsigned int _index) const {
+    return bufferStatus_[_index].time;
+  }
+  inline 
+  void
+  BufferManager::bufferTimeStamp(unsigned int _index, ACE_Time_Value const& _stamp){
+    bufferStatus_[_index].time = _stamp;
+  }
+};
+
+#endif // Video_BufferManager_h
