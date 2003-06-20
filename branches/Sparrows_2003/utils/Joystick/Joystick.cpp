ec8722a9aa6193b2005a93410194049c218378dc
rr4 <rr4@7138bf71-271b-42e2-acd4-47588d9ec08a>
2003-06-20 17:53:08 +0000

    new features

--- branches/Sparrows_2003/utils/Joystick/Joystick.cpp
+++ branches/Sparrows_2003/utils/Joystick/Joystick.cpp
@@ -28,9 +28,9 @@ EventHandler::EventHandler(Miro::Motion_var _motion, Miro::Kicker_var _kicker, b
         kickeron_ = _kickeron;
 	maxRotation = 3.14; 
 	maxTranslation = 1000;
-	
+
 	//Maximal zulässige Translation und Rotation holen
-	
+
 	CORBA::Long minTrans;
 	CORBA::Long maxTrans;
 	CORBA::Double minRot;
@@ -41,47 +41,53 @@ EventHandler::EventHandler(Miro::Motion_var _motion, Miro::Kicker_var _kicker, b
 	minRotation = minRot;
 	maxRotation = maxRot;
         cout << "MaxTranslation: " << maxTranslation << endl;
-        cout << "MaxRotation: " << maxRotation << endl; 
-	
+        cout << "MaxRotation: " << maxRotation << endl;
+	OldVelocity.translation = 0;
+	OldVelocity.rotation = 0.0;
+
+	CurVelocity.translation = 0;
+	CurVelocity.rotation = 0.0;
+
+
 }
 
 int EventHandler::handle_input(ACE_HANDLE){
-    
-    
+
+
     //Joystick-Event anlegen
-    
+
     js_event e;
 
-    
+
     //Geschwindigkeit auf vorherige Werte setzen
-    
-    Miro::VelocityIDL velocity;
-    velocity.rotation = oldrot;
-    velocity.translation = oldtrans;
-    
+
+
+    CurVelocity.rotation = oldrot;
+    CurVelocity.translation = oldtrans;
+
     //Joystick-Device auslesen
-    
+
     read(fd, &e, sizeof(struct js_event));
-    
-    
-    
+
+
+
     //falls Button gedrückt
-    
+
     if(e.type == JS_EVENT_BUTTON){
-       
+
        //Button 0
-       
+
        if(e.number == 0) {
        	oldrot = 0.0;
        	oldtrans = 0;
-       	velocity.rotation = 0.0;
-       	velocity.translation = 0;
-     	motion_->setVelocity(velocity);
-       	
+       	CurVelocity.rotation = 0.0;
+       	CurVelocity.translation = 0;
+     	//motion_->setVelocity(velocity);
+
        }
-       
+
        //Button 1
-       
+
        if(e.number == 1) {
          if(kickeron_){
 	    Miro::TimeIDL time;
@@ -90,43 +96,54 @@ int EventHandler::handle_input(ACE_HANDLE){
 	    kicker_->kick(time);
 	 }
        }
-    
+
     }
-    
-    
+
+
     //falls Joystick bewegt
-    
+
     if(e.type == JS_EVENT_AXIS){
-       
+
        //vertikale Richtung
-       
+
        if (e.number == 1){
           
-	  velocity.translation = -(maxTranslation * (long)e.value)/(32767);
+	  CurVelocity.translation = -(maxTranslation * (long)e.value)/(32767);
 
-	  oldtrans = velocity.translation;
+	  oldtrans = CurVelocity.translation;
 	  cout << maxTranslation << " " << maxRotation << endl;
-          motion_->setVelocity(velocity);
-          cout << "Achse 1 " << e.value << " Geschw: " << velocity.translation
-          << endl;	
+          //motion_->setVelocity(velocity);
+          cout << "Achse 1 " << e.value << " Geschw: " << CurVelocity.translation
+          << endl;
        }
-       
+
        //horizontale Richtung
-       
+
        if (e.number == 0){
-          
-	  velocity.rotation = -0.8*(double)(e.value)*maxRotation/32767.0;
-	  oldrot = velocity.rotation;
-	  motion_->setVelocity(velocity);
+
+	  CurVelocity.rotation = -0.8*(double)(e.value)*maxRotation/32767.0;
+	  oldrot = CurVelocity.rotation;
+	  //motion_->setVelocity(velocity);
 	  cout << "Achse 0 " << e.value << " Geschw: " <<
-          velocity.rotation << endl;
-       }	
-		
+          CurVelocity.rotation << endl;
+       }
+
     }
     return 0;
 }
 
+int EventHandler::handle_timeout(const ACE_Time_Value&, const void *)
+{
+   if (OldVelocity.translation != CurVelocity.translation || OldVelocity.rotation != CurVelocity.rotation){
+      OldVelocity.translation = CurVelocity.translation;
+      OldVelocity.rotation = CurVelocity.rotation;
+      motion_->setVelocity(CurVelocity);
 
+   }
+
+  cout << "********************************handle timeout*********************" << endl;
+  return 0;
+}
 
 
 
@@ -134,7 +151,7 @@ int
 main(int argc, char *argv[])
 {
     //Client erzeugen
-    
+
     Miro::Client client(argc, argv);
 
 
@@ -144,7 +161,7 @@ main(int argc, char *argv[])
     
     bool kickeron = true;
     
-    
+
     //Parameter-Parsing
     for(int i = 1; i < argc; i++){
       if(strcmp("-dev", argv[i])==0){
@@ -206,7 +223,14 @@ main(int argc, char *argv[])
     //EventHandler beim reactor für Handle registrieren und den reactor starten
     
     reactor->register_handler((ACE_HANDLE) fd, &eventHandler, ACE_Event_Handler::READ_MASK);
+
+    ACE_Time_Value delay;
+    delay.set(0,0);
+
+    ACE_Time_Value interval;
+    interval.set(0, 100000);
+    reactor->schedule_timer(&eventHandler, NULL, delay, interval);
     reactor->run_reactor_event_loop();
     return 0;
-    
+
 }
--- branches/Sparrows_2003/utils/Joystick/Joystick.h
+++ branches/Sparrows_2003/utils/Joystick/Joystick.h
@@ -12,7 +12,10 @@ class EventHandler : public ACE_Event_Handler {
 	private:
 	   Miro::Motion_var motion_;
 	   Miro::Kicker_var kicker_;
-	   
+
+	   Miro::VelocityIDL OldVelocity;
+	   Miro::VelocityIDL CurVelocity;
+
 	   ACE_HANDLE fd;
 	   static double oldrot;
 	   static long oldtrans;
@@ -25,7 +28,8 @@ class EventHandler : public ACE_Event_Handler {
     	public:
 	   EventHandler(Miro::Motion_var _motion, Miro::Kicker_var _kicker, bool
 	   _kickeron, ACE_HANDLE fd_);
-	   virtual int handle_input(ACE_HANDLE handle);	
+	   virtual int handle_input(ACE_HANDLE handle);
+	   virtual int handle_timeout(const ACE_Time_Value&, const void *);
     
 };
 
