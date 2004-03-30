ed9ef15c24c08d2a081ccbbf1217e35bbbdc416e
speedy <speedy@7138bf71-271b-42e2-acd4-47588d9ec08a>
2004-03-30 14:44:03 +0000

    Integrated new stuff...

--- trunk/src/video/VideoFilterRGBtoSeg.cpp
+++ trunk/src/video/VideoFilterRGBtoSeg.cpp
@@ -51,29 +51,39 @@ namespace Video
 
     unsigned int length = inputFormat_.width * inputFormat_.height;
     float threshold = (params_ != NULL)?(params_->threshold):80.0;
+
+    int dbg_counter = 0;
     
     std::cout << "VideoFilterRGBtoSeg threshold: " << threshold << std::endl;
 
+    
     for(unsigned int x = 0; x < length; ++x){
 
-          *(tgt_img++) =  (r_lookup[*(src_img++)] +  g_lookup[*(src_img++)] + b_lookup[*(src_img++)] < threshold)?0:255;
+          *(tgt_img++) =  r_lookup[*(src_img++)] +  g_lookup[*(src_img++)] + b_lookup[*(src_img++)];
 
     }
     
-    /*unsigned int * int_p = bitImage_;
+    bzero(bitImage_, 360 * 4 * 4);
+    unsigned int * int_p = bitImage_;
     unsigned int * tmp_p;
     unsigned int mask;
     
     unsigned int zaehler = 0;
     
+    src_img = inputBuffer();
+    tgt_img = outputBuffer();
     int rest = inputFormat_.height % 32;
     if(rest != 0){
        tmp_p = bitImage_;
        for(int w = 0; w < rest; w++){
           int_p = tmp_p;
           for(int z = 0; z < inputFormat_.width; z++){
-            *int_p << 1;
-	    mask =  (r_lookup[*(src_img++)] +  g_lookup[*(src_img++)] + b_lookup[*(src_img++)] < 80.0)?0:0x00000001;
+            *int_p = (*int_p << 1);
+	    mask =  (r_lookup[*(src_img++)] +  g_lookup[*(src_img++)] + b_lookup[*(src_img++)] < threshold)?1:0;
+	    
+	    if(mask)
+		*(tgt_img) = 127;
+	    tgt_img++;
             *int_p |= mask;
             int_p++;
    	    zaehler++;
@@ -89,77 +99,86 @@ namespace Video
        for(int w = 0; w < 32; w++){
           int_p = tmp_p;
           for(unsigned int z = 0; z < inputFormat_.width; z++){
-             *int_p << 1;
-             mask =  (r_lookup[*(src_img++)] +  g_lookup[*(src_img++)] + b_lookup[*(src_img++)] < 80.0)?0:0x00000001;
+             *int_p = (*int_p << 1);
+             mask =  (r_lookup[*(src_img++)] +  g_lookup[*(src_img++)] + b_lookup[*(src_img++)] < threshold)?1:0;
+	     
+             if(mask)
+		*(tgt_img) = 127;
+	     tgt_img++;
             *int_p |= mask;
             int_p++;
             zaehler++;
 	  }
        }
     }
-    
+   
     numInts += ptroff;
     unsigned int first, pre, actual;
     for(int i = 0; i < numInts; i++){
        int_p = bitImage_ + i*inputFormat_.width;
        pre = *int_p;
        first = *int_p;
-       *int_p = *int_p | *(int_p + inputFormat_.widht - 1) | *(int_p + 1);
+       *int_p = *int_p & *(int_p + inputFormat_.width - 1) & *(int_p + 1);
        int_p++; 
        for(int j = 1; j < inputFormat_.width - 1; j++){
           actual = *int_p; 
-          *int_p = pre | actual | *(int_p + 1);
+          *int_p = pre & actual & *(int_p + 1);
 	  pre = actual;
 	  int_p++;
        
        }
-       *int_p = pre | *int_p | first;
+       *int_p = pre & *int_p & first;
     }
     
     
     int index;
     int tmpInd;
     int z = 0;
+
+    tgt_img = outputBuffer();
     
     for(int i = 0; i < inputFormat_.width; i++){
        int_p = bitImage_ + (numInts -1)*inputFormat_.width + i;
        z = 0;
-       index = 0;
+       index = -1;
        tmpInd = 0;
        while(*int_p == 0 && z < numInts){
           int_p -= inputFormat_.width;
 	  z++;
 	  tmpInd += 32;
-       
        }
        if(z == numInts){
           distanceProfile_[i] = 5000;
        }
        else{
-          for(int j = z; j < numInts; k++){
-	     if(*int_p != 0){
+	  dbg_counter++;
+
+          for(int j = z; j < numInts; j++) {
+	     if(*int_p != 0) {
 	        actual = *int_p;
 		int range = (j == numInts - 1)?rest-2:30;
-		for(int k = 0; k < range; k++){
-		   if(actual & 0x00000007 == 0x000000007)
+		for(int k = 0; k < range; k++) {
+		   if(actual & 0x00000007 == 0x00000007) {
 		      index = tmpInd;
 		      break;
 		   }   
                    actual = actual >> 1;
 		   tmpInd++;		
 		}
-		if((index == 0) &&(j < numInts - 1) && 
+		if((index == -1) &&(j < numInts - 1) && 
 		   (actual & 0x00000003 == 0x00000003) &&
-		   (*(int_p - inputFormat_.width) & 0x00000001 = 0x00000001))
+		   (*(int_p - inputFormat_.width) & 0x00000001 == 0x00000001)) {
 		      index = tmpInd;
-		else if(index == 0){
+		}
+		else if(index == -1) {
 		   actual = actual >> 1;
 		   tmpInd++;
 		   if((j < numInts - 1) &&
 		      (actual & 0x00000001 == 0x00000001) &&
-		      (*(int_p - inputFormat_.width) & 0x00000003 = 0x00000003))
+		      (*(int_p - inputFormat_.width) & 0x00000003 == 0x00000003)) {
 		         index = tmpInd;
-	           }
+		   }
+	           
 		
 		} 
 	     }    
@@ -168,7 +187,8 @@ namespace Video
 	        tmpInd +=32;
 	     }
 	     
-	     if(index != 0)
+	     
+	     if(index != -1)
 	        break;   
              
 	  }
@@ -176,18 +196,20 @@ namespace Video
           
                 
        }
-       if(index == 0)
-          distanceProfile_[i] = 5000;
-       else
-          distanceProfile_[i] = distLookup_[index];
+       if(index == -1)
+       	 distanceProfile_[i] = 5000;
+       else {
+       	 distanceProfile_[i] = distLookup_[index];
+	 for(int m = 0; m < inputFormat_.height - index; m++) {
+	         *(tgt_img + (m * inputFormat_.width) + i) = 255; 
+	 }
+       }
     
     }
     
-    
+    std::cout << "RGBtoSeg Debug Counter: " << dbg_counter << std::endl;
     
     //std::cout << "Debug output: RGBtoSeg Counter: " << zaehler << std::endl;
-        
-*/
 
   }
   
