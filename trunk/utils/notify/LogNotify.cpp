fdbc9db62d7372b0cd751a5925d115193120d52f
hutz <hutz@7138bf71-271b-42e2-acd4-47588d9ec08a>
2004-07-16 14:54:18 +0000

    removing the log notify consumer, as it resides now in src/miro

--- trunk/utils/notify/LogNotify.cpp
+++ trunk/utils/notify/LogNotify.cpp
@@ -2,138 +2,80 @@
 //
 // This file is part of Miro (The Middleware For Robots)
 //
-// (c) 1999, 2000, 2001
+// (c)  2001, 2002, 2003, 2004
 // Department of Neural Information Processing, University of Ulm, Germany
 //
 // $Id$
 // 
 //////////////////////////////////////////////////////////////////////////////
 
-
-#include "LogNotifyConsumer.h"
-
+#include "miro/LogNotifyConsumer.h"
 #include "miro/Server.h"
-#include "miro/Utils.h"
-
-#include <ace/Date_Time.h>
-
-#include <sstream>
-
-using std::cout;
-using std::cerr;
-using std::endl;
-using std::flush;
-using std::cin;
-using std::string;
-using std::ostringstream;
-
-using CosNotification::EventTypeSeq;
-using CosNotification::StructuredEvent;
-using CosNotifyChannelAdmin::EventChannel;
-using CosNotifyChannelAdmin::EventChannel_ptr;
-using CosNotifyChannelAdmin::EventChannel_var;
-
-namespace 
-{
-  string path()
-  {
-    char * miro_log = ACE_OS::getenv("MIRO_LOG");
-    string p;
-
-    if (miro_log)
-	p = miro_log;
-
-    if (p.size() > 0 && p[p.size() - 1] != '/')
-      p += "/";
-    return p;
-  }
-
-
-  string createFileName()
-  {
-    ACE_Date_Time dt;
-
-    ostringstream ostr;
-
-    ostr << dt.year() << ".";
-    ostr.width(2);
-    ostr.fill('0');
-    ostr << dt.month() << ".";
-    ostr.width(2);
-    ostr.fill('0');
-    ostr << dt.day() << "-";
-    ostr.width(2);
-    ostr.fill('0');
-    ostr << dt.hour() << ".";
-    ostr.width(2);
-    ostr.fill('0');
-    ostr << dt.minute()
-	 << ".log";
-
-    return ostr.str();
-  }
-};
+#include "miro/ConfigDocument.h"
+#include "miro/TimeHelper.h"
+#include "miro/Log.h"
 
 int
 main(int argc, char *argv[])
 {
-  string fileName;
+  int rc = 1;
+  std::string fileName;
 
   // Parameters to be passed to the services
-  Miro::RobotParameters robotParameters;
-  LogNotifyParameters * parameters = LogNotifyParameters::instance();
+  Miro::RobotParameters * robotParameters = Miro::RobotParameters::instance();
+  Miro::LogNotifyParameters * parameters = Miro::LogNotifyParameters::instance();
 
   // Config file processing
   Miro::ConfigDocument * config = 
     new Miro::ConfigDocument(argc, argv);
   config->setSection("Robot");
-  config->getParameters("Robot", robotParameters);
+  config->getParameters("Robot", *robotParameters);
   config->setSection("Notification");
   config->getParameters("Logging", *parameters);
   delete config;
+  
+  MIRO_DBG_OSTR(MIRO , LL_PRATTLE , "  robot parameters:\n" << robotParameters);
+  MIRO_DBG_OSTR(MIRO , LL_PRATTLE , "  logging parameters:\n" << parameters);
 
-#ifdef DEBUG
-    cout << "parameters:" << endl << parameters << endl;
-#endif
-
-  cout << "Initialize server daemon." << flush;
-  Miro::Server server(argc, argv, robotParameters);
+  MIRO_LOG(LL_NOTICE,"Initialize server daemon.");
+  Miro::Server server(argc, argv);
 
   // override default logfile name with command line parameter
   if (argc > 1)
     fileName = argv[1];
-  else
-    fileName = path() + server.namingContextName + "_" + createFileName();
 
   try {
     // The one channel that we create using the factory.
-    EventChannel_var ec(server.resolveName<EventChannel>("EventChannel"));
+    CosNotifyChannelAdmin::EventChannel_var 
+      ec(server.resolveName<CosNotifyChannelAdmin::EventChannel>("EventChannel"));
     
     // The consumer, that gets the events
-    LogNotify pushConsumer(server,
-			   ec.in(), 
-			   server.namingContextName,
-			   fileName,
-			   parameters);
+    Miro::LogNotifyConsumer pushConsumer(server,
+					 ec.in(), 
+					 server.namingContextName,
+					 fileName,
+					 *parameters,
+					 false);
     
-    cout << "Loop forever handling events." << endl;
-    server.run();
-    cout << "Server stoped, exiting." << endl;
+    try {
+      MIRO_LOG(LL_NOTICE , "Loop forever handling events." );
+      server.run(1);
+      MIRO_LOG(LL_NOTICE , "Server loop ended, exiting." );
+      rc = 0;
+    }
+    catch (const Miro::Exception& e) {
+      MIRO_LOG_OSTR(LL_CRITICAL , "Uncought Miro excepition: " << e);
+    }
 
     server.detach(1);
+
+    MIRO_LOG(LL_NOTICE , "shutting down server.");
     pushConsumer.disconnect();
-    
-    cout << "shutting down server." << endl;
     server.shutdown();
     server.wait();
   }
   catch (const CORBA::Exception & e) {
-    cerr << "Uncaught CORBA exception: " << e << endl;
-    return 1;
+    MIRO_LOG_OSTR(LL_CRITICAL , "Uncaught CORBA exception: " << e);
   }
-  catch (...) {
-    cerr << "Uncaught exception!" << endl;
-    return 1;
-  }
-  return 0;
+  return rc;
 }
