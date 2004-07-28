2eed17c5170f3b649c60e5d332c3de9305a44024
hutz <hutz@7138bf71-271b-42e2-acd4-47588d9ec08a>
2004-07-28 15:02:28 +0000

    improving the Item base class for usage with the config editor

--- trunk/utils/widgets/Item.cpp
+++ trunk/utils/widgets/Item.cpp
@@ -2,7 +2,7 @@
 //
 // This file is part of Miro (The Middleware For Robots)
 //
-// (c) 2003
+// (c) 2004
 // Department of Neural Information Processing, University of Ulm, Germany
 //
 // $Id$
@@ -11,18 +11,40 @@
 
 #include "Item.h"
 
-#include <qlistview.h>
+#include "miro/Log.h"
+
+#include <qstring.h>
 #include <qobjectlist.h>
+#include <qlistview.h>
 
 Item::ItemMap Item::itemMap_;
 
-Item::Item(QObject * _parent, char const * _name) :
-  Super(_parent, _name),
-  listViewItem_(NULL)
-{}
+//----------------------------------------------------------------------------
+// public methods
+//----------------------------------------------------------------------------
 
-Item::~Item() 
+Item::Item(QListViewItem * _parentItem, QListViewItem * _pre,
+	   QObject * _parent, const char * _name) :
+  Super(_parent, _name),
+  listViewItem_(new QListViewItem(_parentItem, _pre))
 {
+  listViewItem()->setText(0, name());
+  itemMap_.insert(std::make_pair(listViewItem_, this));
+}
+
+Item::Item(QListView * _view, QListViewItem * _pre,
+	   QObject * _parent, const char * _name) :
+  Super(_parent, _name),
+  listViewItem_(new QListViewItem(_view, _pre))
+{
+  listViewItem()->setText(0, name());
+  itemMap_.insert(std::make_pair(listViewItem_, this));
+}
+
+Item::~Item()
+{
+  //  cout << name() << " deleting children" << endl;
+
   if (children()) {
     QObjectList childList = *children();
     QObject * c = childList.first();
@@ -32,8 +54,12 @@ Item::~Item()
     }
   }
 
-  itemMap_.erase(listViewItem_);
+  //  cout << name() << " deleting listviewitem" << endl;
+
   delete listViewItem_;
+  itemMap_.erase(listViewItem_);
+
+  //  cout << name() << " deleting" << endl;
 }
 
 void
@@ -41,16 +67,106 @@ Item::update()
 {
 }
 
-void
-Item::setListViewItem(QListViewItem * _listViewItem)
+void 
+Item::contextMenu(QPopupMenu&)
 {
-  if (listViewItem_ != NULL)
-    itemMap_.erase(listViewItem_);
-  listViewItem_ = _listViewItem;
-  itemMap_.insert(std::make_pair(listViewItem_, this));
+}
+
+//------------------------------------------------------------------------------
+// public slots
+//------------------------------------------------------------------------------
+
+void 
+Item::up()
+{
+  moveUp();
 }
 
 void
-Item::contextMenu(QPopupMenu&)
+Item::down()
 {
+  moveDown();
+}
+
+void
+Item::moveUp()
+{
+  // find the pre predecessor of the item
+  QListViewItem * prePre = NULL;
+  QListViewItem * pre = NULL;
+  QListViewItem * self = NULL;
+
+  // get the parents first child
+  self = (listViewItem()->parent() == NULL)?
+    listViewItem()->listView()->firstChild() :
+    listViewItem()->parent()->firstChild();
+
+  // find ourselves
+  while (self != listViewItem()) {
+    MIRO_ASSERT(self != NULL);
+
+    prePre = pre;
+    pre = self;
+    self = self->nextSibling();
+  }
+
+  if (pre != NULL) {
+    if (prePre != NULL) {
+      listViewItem()->moveItem(prePre);
+    }
+    else {
+      pre->moveItem(listViewItem());
+    }
+  }
+}
+
+void
+Item::moveDown()
+{
+  QListViewItem * succ = listViewItem()->nextSibling();
+  if (succ != NULL) {
+    listViewItem()->moveItem(succ);
+  }
+}
+
+Item *
+Item::predecessor()
+{
+  // find the pre predecessor of the item
+  QListViewItem * prePre = NULL;
+  QListViewItem * pre = NULL;
+  QListViewItem * self = NULL;
+
+  // get the parents first child
+  self = (listViewItem()->parent() == NULL)?
+    listViewItem()->listView()->firstChild() :
+    listViewItem()->parent()->firstChild();
+
+  // find ourselves
+  while (self != listViewItem()) {
+    MIRO_ASSERT(self != NULL);
+
+    prePre = pre;
+    pre = self;
+    self = self->nextSibling();
+  }
+
+  return itemFromListViewItem(pre);
+}
+
+Item *
+Item::successor()
+{
+  return itemFromListViewItem(listViewItem_->nextSibling());
+}
+
+Item *
+Item::itemFromListViewItem(QListViewItem * _lvi)
+{
+  if (_lvi == NULL)
+    return NULL;
+
+  ItemMap::const_iterator i = itemMap_.find(_lvi);
+  MIRO_ASSERT(i != itemMap_.end());
+  return i->second;
 }
--- trunk/utils/widgets/Item.h
+++ trunk/utils/widgets/Item.h
@@ -2,48 +2,106 @@
 //
 // This file is part of Miro (The Middleware For Robots)
 //
-// (c) 2003
+// (c) 2004
 // Department of Neural Information Processing, University of Ulm, Germany
 //
 // $Id$
 // 
 //////////////////////////////////////////////////////////////////////////////
-#ifndef notifyObserver_Item
-#define notifyObserver_Item
+#ifndef Item_h
+#define Item_h
 
 #include <qobject.h>
 
 #include <map>
 
-class QListViewItem;
+// forward declarations
 class QPopupMenu;
+class QListView;
+class QListViewItem;
 
+//! This class represents a BAP item in the XML tree.
+/**
+ * It encapsulates the xml processing of the item.
+ */
 class Item : public QObject
 {
-  Q_OBJECT
+  Q_OBJECT;
+
+  //----------------------------------------------------------------------------
+  // private types
+  //----------------------------------------------------------------------------
 
   typedef QObject Super;
 
- public:
-  typedef std::map<QListViewItem *, Item*> ItemMap;
+public:
+  //----------------------------------------------------------------------------
+  // public types
+  //----------------------------------------------------------------------------
+  typedef std::map<QListViewItem *, Item *> ItemMap;
 
-  Item(QObject * _parent, char const * _name);
-  ~Item();
+  //----------------------------------------------------------------------------
+  // public methods
+  //----------------------------------------------------------------------------
 
-  virtual void update();
-  virtual void contextMenu(QPopupMenu& _menu);
-  
+  //! Initializing constructor.
+  Item(QListViewItem * _parentItem, QListViewItem * _pre = NULL,
+       QObject * _parent = NULL, const char * _name = NULL);
+  Item(QListView * _view, QListViewItem * _pre = NULL,
+       QObject * _parent = NULL, const char * _name = NULL);
+  virtual ~Item();
+
   QListViewItem * listViewItem();
-  void setListViewItem(QListViewItem * _listViewItem);
+  QListViewItem const * listViewItem() const;
+
+  //! Move item up in list view.
+  virtual void moveUp();
+  //! Move item down in list view.
+  virtual void moveDown();
+  //! Update the item tree.
+  virtual void update();
+  //! Populate the provided context menu with entries for the item.
+  virtual void contextMenu(QPopupMenu& _menu);
+
+  //----------------------------------------------------------------------------
+  // static public methods
+  //----------------------------------------------------------------------------
 
   static ItemMap const& itemMap();
+  static Item * itemFromListViewItem(QListViewItem * _lvi);
+
+public slots:
+  //----------------------------------------------------------------------------
+  // public sots
+  //----------------------------------------------------------------------------
+
+  //! Move the element before the predecessor element.
+  void up();
+  //! Move the element behind the successor element.
+  void down();
 
 protected:
+  //----------------------------------------------------------------------------
+  // protected methods
+  //----------------------------------------------------------------------------
+  Item * predecessor();
+  Item * successor();
+
+  //----------------------------------------------------------------------------
+  // protected members
+  //----------------------------------------------------------------------------
   static ItemMap itemMap_;
 
 private:
-  //! List view for displaying the tree.
+  //----------------------------------------------------------------------------
+  // private members
+  //----------------------------------------------------------------------------
   QListViewItem * listViewItem_;
+
+  //----------------------------------------------------------------------------
+  // hidden methods
+  //----------------------------------------------------------------------------
+  Item(Item const&);
 };
 
 inline
@@ -51,11 +109,15 @@ QListViewItem *
 Item::listViewItem() {
   return listViewItem_;
 }
-
+inline
+QListViewItem const *
+Item::listViewItem() const {
+  return listViewItem_;
+}
 inline
 Item::ItemMap const&
 Item::itemMap() {
   return itemMap_;
 }
 
-#endif //notifyObserver_Item
+#endif
