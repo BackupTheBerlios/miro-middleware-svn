7533e6a18f9ce4aa08f2e9d851f40d90d807e07c
hutz <hutz@7138bf71-271b-42e2-acd4-47588d9ec08a>
2006-07-10 22:01:06 +0000

    beautifying configuration parameters example

--- trunk/examples/params/ParameterExample.cpp
+++ trunk/examples/params/ParameterExample.cpp
@@ -2,7 +2,7 @@
 //
 // This file is part of Miro (The Middleware For Robots)
 //
-// (c) 2003
+// (c) 2003, 2006
 // Department of Neural Information Processing, University of Ulm, Germany
 //
 // $Id$
@@ -11,42 +11,56 @@
 
 #include "Parameters.h"
 
-#include "miro/Utils.h"
+#include "miro/Log.h"
+#include "miro/Configuration.h"
 #include "miro/Exception.h"
 
-using std::cout;
-using std::cerr;
-using std::endl;
+using namespace std;
 
 int
 main(int argc, char *argv[])
 {
   int rc = 0;
 
-  Test::MyParameters * params = Test::MyParameters::instance();
+  // command line paramter parsing
+  // -> increase log level for verbosity (-MLL 7)
+  Miro::Log::init(argc, argv);
+  // -> specify the config file (-MCF Config.xml)
+  Miro::Configuration::init(argc, argv);
+  // Get global configuration document
+  Miro::ConfigDocument * config = Miro::Configuration::document();
 
+  // Read parameters from document...
   try {
-    // Config file processing
-    Miro::ConfigDocument * config =
-      new Miro::ConfigDocument(argc, argv, "Config.xml");
-    config->setSection("test");
-    config->getParameters("Test", *params);
-    delete config;
+    // global parameters
+    // -> those are accessed through singletons
+    Test::MyParameters * params = Test::MyParameters::instance();
+    Test::MoreParameters * more_params = Test::MoreParameters::instance();
 
-    cout << "  test paramters:" << endl << *params << endl << endl;
+    // individual parameters
+    // -> those are locally allocated
+    Test::FriendsParameters tom;
+    Test::FriendsParameters cherry;
 
-    QDomDocument doc("MiroConfigDocument");
-    QDomElement e = doc.createElement("config");
-    doc.appendChild(e);
-    QDomElement f = doc.createElement("group");
-    f.setAttribute("name", "test");
-    e.appendChild(f);
+    // selecting the paramter section
+    config->setSection("My_test_section");
 
-    QDomElement g = (*params) >>= f;
-    g.setAttribute("name", "Test");
+    // reading global parameters (if present)
+    // -> no name required
+    config->getParameters("Test::MyParameters", *params);
+    config->getParameters("Test::MoreParameters", *more_params);
 
-    QString s = doc.toString();
-    std::cout << s;
+    // reading individual parameter settings (if present)
+    // -> referred by name
+    config->getType("Test::FriendsParameters", "Tom", tom);
+    config->getType("Test::FriendsParameters", "Cherry", cherry);
+
+    // debug output 
+    cout << "  my paramters:" << endl << *params << endl << endl;
+    cout << "  more paramters:" << endl << *more_params << endl << endl;
+    cout << "  tom's paramters:" << endl << tom << endl << endl;
+    cout << "  cherry's paramters:" << endl << cherry << endl << endl;
+
   }
   catch (const Miro::CException& e) {
     cerr << "Miro exception: " << e << endl;
